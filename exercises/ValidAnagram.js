5f2172d72039f7999cc48ad8d25a4da0d637a836
Brenda Ferrufino <ferrufinob@gmail.com>
2021-09-29 23:00:50 -0400

    valid anagram second solution

--- exercises/ValidAnagram.js
+++ exercises/ValidAnagram.js
@@ -9,6 +9,7 @@ function validAnagram(s1, s2) {
   //   count number of occurrences
   for (let value of s1) {
     frequencyCounter1[value] = (frequencyCounter1[value] || 0) + 1;
+    console.log(value);
   }
   for (let value of s2) {
     frequencyCounter2[value] = (frequencyCounter2[value] || 0) + 1;
@@ -21,7 +22,7 @@ function validAnagram(s1, s2) {
       return false;
     }
     // check if there is a match
-    console.log(frequencyCounter2[key], frequencyCounter1[key]);
+    // console.log(frequencyCounter2[key], frequencyCounter1[key]);
     if (frequencyCounter2[key] !== frequencyCounter1[key]) {
       return false;
     }
@@ -29,6 +30,37 @@ function validAnagram(s1, s2) {
   return true;
 }
 
-console.log(validAnagram("aaz", "zza")); // false
-console.log(validAnagram("awesome", "awesom")); // false
-console.log(validAnagram("qwerty", "qeywrt")); // true
+// console.log(validAnagram("aaz", "zza")); // false
+// console.log(validAnagram("awesome", "awesom")); // false
+// console.log(validAnagram("qwerty", "qeywrt")); // true
+// console.log(validAnagram("texttwisttime", "timetwisttext")); // true
+
+// Video Solution
+
+function validAnagram2(first, second) {
+  if (first.length !== second.length) return false;
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    // if letter exists, increment, otherwise set to 1
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+}
+
+console.log(validAnagram2("aaz", "zza")); // false
+console.log(validAnagram2("awesome", "awesom")); // false
+console.log(validAnagram2("qwerty", "qeywrt")); // true
+console.log(validAnagram2("texttwisttime", "timetwisttext")); // true
